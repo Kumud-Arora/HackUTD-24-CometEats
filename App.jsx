bdd8a3e911ac27ae0f6fd439b6b30a26843d86d7
Kumud-Arora <149901769+Kumud-Arora@users.noreply.github.com>
2024-11-17 11:22:35 -0600

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,14 +1,30 @@
 import { Routes, Route } from 'react-router-dom';
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header.jsx'
-//import Login from './Login.jsx'
+import Menu from './Menu.jsx'
+import Order from './Order.jsx'
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [selectedRestaurant, setSelectedRestaurant] = useState(null);
+
   return (
-      <div style={{ border: "50px solid white", height: "87vh", width: "171vh", margin: "0" }}>
-          <Header />
-          
-      </div>
+    <div style={{ border: "50px solid white", height: "87vh", width: "171vh", margin: "0" }}>
+      <Header isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+      {isLoggedIn && !selectedRestaurant && (
+        <div>
+          <h2>Choose a restaurant:</h2>
+          <button onClick={() => setSelectedRestaurant('panda')}>Panda Express</button>
+          <button onClick={() => setSelectedRestaurant('einstein')}>Einstein Bros. Bagels</button>
+        </div>
+      )}
+      {isLoggedIn && selectedRestaurant && (
+        <Routes>
+          <Route path="/" element={<Menu restaurant={selectedRestaurant} />} />
+          <Route path="/order" element={<Order />} />
+        </Routes>
+      )}
+    </div>
   );
 }
 
